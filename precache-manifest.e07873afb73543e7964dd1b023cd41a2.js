a9a23d02837976b128a03cc76ab98040b4626ba2
Nite <nitearie@gmail.com>
2019-12-10 01:27:35 +0700

    Updates

---
+++ precache-manifest.e07873afb73543e7964dd1b023cd41a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d152e563a6c3704fc43f1c88395d8b3b",
+    "url": "/smart-brain-api/index.html"
+  },
+  {
+    "revision": "63ee03d1eed38a6d7ab8",
+    "url": "/smart-brain-api/static/css/main.6250aa7e.chunk.css"
+  },
+  {
+    "revision": "ecfe2428e8c05bdb63f8",
+    "url": "/smart-brain-api/static/js/2.43a386bd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/smart-brain-api/static/js/2.43a386bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "63ee03d1eed38a6d7ab8",
+    "url": "/smart-brain-api/static/js/main.17ddd30f.chunk.js"
+  },
+  {
+    "revision": "2c3a9efb36d7cc17f29f",
+    "url": "/smart-brain-api/static/js/runtime-main.5e364480.js"
+  },
+  {
+    "revision": "035bc20bb0716eaa84731479dade7827",
+    "url": "/smart-brain-api/static/media/brain.035bc20b.png"
+  }
+]);
